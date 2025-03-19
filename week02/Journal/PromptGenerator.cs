c4fd6bc753dcb26cf8affe897a28fe695a3127bd
coyegoke <coyegoke@byupathway.edu>
2025-03-19 09:42:02 +0100

    Promptgenerator completed

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    // List of possible prompts
+    private List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    private Random random = new Random();  // Random number generator
+
+    // Method to get a random prompt
+    public string GetRandomPrompt()
+    {
+        int index = random.Next(prompts.Count);
+        return prompts[index];
+    }
+}
