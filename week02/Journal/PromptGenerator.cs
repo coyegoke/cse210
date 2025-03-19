5169590334705cd3c52918e88cb9466f2db1fed7
coyegoke <coyegoke@byupathway.edu>
2025-03-19 09:53:54 +0100

    More explanatory comments

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    // List of possible prompts
+    // List of possible prompts (given)
     private List<string> prompts = new List<string>
     {
         "Who was the most interesting person I interacted with today?",
@@ -15,7 +15,7 @@ public class PromptGenerator
 
     private Random random = new Random();  // Random number generator
 
-    // Method to get a random prompt
+    // Method to get a random prompt from the lists of prompts available.
     public string GetRandomPrompt()
     {
         int index = random.Next(prompts.Count);
