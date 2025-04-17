c95b43bd745d67eea519777bc61624cc362ce121
coyegoke <coyegoke@byupathway.edu>
2025-04-17 20:52:43 +0100

    Program class updated

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new RunningActivity("03 Nov 2022", 30, 3.0, false),  // miles
+            new CyclingActivity("04 Nov 2022", 45, 20.0, true),   // kph
+            new SwimmingActivity("05 Nov 2022", 60, 40, false)     // miles
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
